f38bd06050e801fddc7d4643632b1f41ad50e68a
ivanpachecos <ivanpachecos@yahoo.com>
2024-03-29 06:37:57 -0400

    prototype

--- main.h
+++ main.h
@@ -1,1 +1,9 @@
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
 int _printf(const char *format, ...);
+int print_char(char c);
+int _putchar(char c);
+int print_str(char *str);
+int print_porcent();
+#endif
