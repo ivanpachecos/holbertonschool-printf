1e40c404de55b0753363a79708f1d9ebdb440a40
kleber89 <benkosisackson@gmail.com>
2024-03-30 18:11:07 -0500

    Update file _printf

--- _printf.c
+++ _printf.c
@@ -1,49 +1,37 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stddef.h>
-/**
-* _printf - main function to print in console
-* @format: array to print and check type
-* Return: count of character printed
-**/
 int _printf(const char *format, ...)
 {
-	int count = -1;
-
-	if (format != NULL)
-	{
-		int i;
-		va_list ar_list;
-		int (*o)(va_list);
-
-		va_start(ar_list, format);
-
-		if (format[0] == '%' && format[1] == '\0')
-			return (-1);
-
-		count = 0;
-
-		for (i = 0; format[i] != '\0'; i++)
-		{
-			if (format[i] == '%')
-			{
-				if (format[i + 1] == '%')
-				{
-					count += _putchar(format[i]);
-					i++;
-				}
-				else if (format[i + 1] != '\0')
-				{
-					o = get_func(format[i + 1]);
-					count += (o ? o(ar_list) : _putchar(format[i]) + _putchar(format[i + 1]));
-					i++;
-				}
-			}
-			else
-				count += _putchar(format[i]);
-		}
-		va_end(ar_list);
-	}
-
-	return (count);
+    va_list args;
+    int count = 0;
+    va_start(args, format);
+    if (format == NULL)
+        return (-1);
+    while (*format != '\0') {
+        if (*format == '%')
+        {
+            format++;
+            switch (*format)
+            {
+                case 'c':
+                    count += print_char(va_arg(args, int));
+                    break;
+                case 's':
+                    count += print_str(va_arg(args, char *));
+                    break;
+                case '%':
+                    count += print_porcent();
+                    break;
+                default:
+                    _putchar(*format);
+                    break;
+            }
+        }
+        else
+            count += _putchar(*format);
+        format++;
+    }
+    va_end(args);
+    return (count);
 }
