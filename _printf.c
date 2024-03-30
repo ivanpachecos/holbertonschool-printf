62e3bc9e3c58314a0e2ae46fbc9fc16de6958450
kleber89 <benkosisackson@gmail.com>
2024-03-30 18:13:50 -0500

    update file _printf

--- _printf.c
+++ _printf.c
@@ -1,37 +1,43 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stddef.h>
+/**
+* _printf - Same with printf function
+* @format: string to remplace %s, %c etc...
+* Return: Always size of output
+*/
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    va_start(args, format);
-    if (format == NULL)
-        return (-1);
-    while (*format != '\0') {
-        if (*format == '%')
-        {
-            format++;
-            switch (*format)
-            {
-                case 'c':
-                    count += print_char(va_arg(args, int));
-                    break;
-                case 's':
-                    count += print_str(va_arg(args, char *));
-                    break;
-                case '%':
-                    count += print_porcent();
-                    break;
-                default:
-                    _putchar(*format);
-                    break;
-            }
-        }
-        else
-            count += _putchar(*format);
-        format++;
-    }
-    va_end(args);
-    return (count);
+	va_list args;
+	int ss = 0;
+	int (*execute_operator)(va_list);
+	int size = 0;
+
+	char currChar;
+	char nextChar;
+
+	if (!format || (format[0] == '%' && strlen(format) == 1))
+		return (-1);
+
+	va_start(args, format);
+
+	while (format[ss])
+	{
+		currChar = format[ss];
+		nextChar = format[ss += 1];
+		execute_operator = get_operator(nextChar);
+
+		if (currChar != '%' || !nextChar || !execute_operator)
+		{
+			size += _putchar(currChar);
+			if (currChar == '%' && nextChar == '%')
+				ss += 1;
+			continue;
+		}
+		size += execute_operator(args);
+		ss += 1;
+	}
+
+	va_end(args);
+	return (size);
 }
