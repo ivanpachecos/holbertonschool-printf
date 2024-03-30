9f4b4d1125cc9ffda31b6d1c47232ff64452084b
ivanpachecos <ivanpachecos@yahoo.com>
2024-03-29 21:53:42 -0400

    function printf

--- _printf.c
+++ _printf.c
@@ -11,5 +11,37 @@
   */
 int _printf(const char *format, ...) 
 {
+	va_list args;
+	int count = 0;
+	va_start(args, format);
 
+    
+	while (*format != '\0') {
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					count += print_char(va_arg(args, int));
+					break;
+				case 's':
+					count += print_str(va_arg(args, char *));
+					break;
+				case '%':
+					count += print_porcent();
+					break;
+				default:
+					_putchar('%');
+					_putchar(*format);
+					count += 2;
+					break;
+			}
+		}
+		else
+			count += _putchar(*format);
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
---
+++ print_char.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * print_char - ..
+  *
+  * @c: ....
+  * Return: one
+  */
+int print_char(char c)
+{
+	_putchar(c);
+	return (1);
+}
