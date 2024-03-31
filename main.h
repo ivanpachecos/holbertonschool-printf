602f15a8e51bf8cbc355911a89e63412a49676ca
ivanpachecos <ivanpachecos@yahoo.com>
2024-03-31 01:59:17 -0500

    writes an character

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+int _printf(const char *format, ...);
+int _putchar(char c);
+#endif
