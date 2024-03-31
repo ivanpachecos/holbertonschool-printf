98754a368437201e580dbc3d3d0826a893d5938a
ivanpachecos <ivanpachecos@yahoo.com>
2024-03-31 00:59:36 -0400

    holberton

--- _printf.c
+++ _printf.c
@@ -1,45 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
-#include <stdarg.h>
-#include <stddef.h>
 
 /**
-* _printf - Same with printf function
-* @format: string to remplace %s, %c etc...
-* Return: Always size of output
-*/
-
+ * _printf - function my printf
+ * @format: string whit format to print
+ *
+ * Return: number of chars that print
+ */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int ss = 0;
-	int (*execute_operator)(va_list);
-	int size = 0;
+	int length = 0;
 
-	char currChar;
-	char nextChar;
-
-	if (!format || (format[0] == '%' && strlen(format) == 1))
+	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
 
-	while (format[ss])
-	{
-		currChar = format[ss];
-		nextChar = format[ss += 1];
-		execute_operator = get_operator(nextChar);
-
-		if (currChar != '%' || !nextChar || !execute_operator)
-		{
-			size += _putchar(currChar);
-			if (currChar == '%' && nextChar == '%')
-				ss += 1;
-			continue;
-		}
-		size += execute_operator(args);
-		ss += 1;
-	}
-
+	length = _print_format(format, args);
 	va_end(args);
-	return (size);
+	return (length);
 }
--- main.h
+++ main.h
@@ -1,9 +1,29 @@
-#ifndef _HEADER_H_
-#define _HEADER_H_
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdarg.h>
 
+/**
+  * struct specifiers - Struct specifiers
+  * @specifier: The conversion specifier
+  * @f: The function pointer
+  */
+typedef struct specifiers
+{
+	char *specifier;
+	int (*f)(va_list args);
+} spc_dt;
+
+int _write(char c);
 int _printf(const char *format, ...);
-int print_char(char c);
-int _putchar(char c);
-int print_str(char *str);
-int print_percent();
+int _print_a_char(va_list args);
+int _print_a_string(va_list args);
+int _print_format(const char *format, va_list args);
+int _print_spec(char format, va_list args);
+int _print_invalid_spec(char prev_format, char format, int count);
+int _print_a_integer(va_list args);
+void _recursion_integer(int a);
+int _print_int_binary(va_list args);
+void _recursion_int_binary(int a);
+int _validate_char(char _type);
+
 #endif
