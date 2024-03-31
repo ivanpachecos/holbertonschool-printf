1f0f6bf92d0e46130bc792b02753fc7b9b3fae4a
ivanpachecos <ivanpachecos@yahoo.com>
2024-03-31 07:30:56 -0400

    file update

--- main.h
+++ main.h
@@ -1,11 +1,29 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdarg.h>
 
-/*
- * Structs, enums and unions definitions
- * Typedefs
- * Function prototypes
- */
-int _printf(const char *format, ...);
+/**
+  * struct specifiers - Struct specifiers
+  * @specifier: The conversion specifier
+  * @f: The function pointer
+  */
+typedef struct specifiers
+{
+	char *specifier;
+	int (*f)(va_list args);
+} spc_dt;
+
 int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list args);
+int _print_a_string(va_list args);
+int print_format(const char *format, va_list args);
+int _print_spec(char format, va_list args);
+int _print_invalid_spec(char prev_format, char format, int count);
+int _print_a_integer(va_list args);
+void _recursion_integer(int a);
+int _print_int_binary(va_list args);
+void _recursion_int_binary(int a);
+int _validate_char(char _type);
+
 #endif
