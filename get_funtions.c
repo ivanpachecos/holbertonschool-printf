a910e49f61ae2d99677bc4659d3cfb59207db363
ivanpachecos <ivanpachecos@yahoo.com>
2024-04-01 01:59:21 -0400

    operations of the functions

---
+++ get_funtions.c
@@ -0,0 +1,92 @@
+#include "main.h"
+
+/**
+ * print_char - Prints a character
+ * @args: A list of variadic arguments
+ *
+ * Return: The length of the character
+ */
+int print_char(va_list args)
+{
+    char character = va_arg(args, int); /* Gets the character from variadic arguments */
+    _putchar(character);
+    return (1);
+}
+
+/**
+ * _print_a_string - Prints a string
+ * @args: A list of variadic arguments
+ *
+ * Return: The length of the string
+ */
+int _print_a_string(va_list args)
+{
+    char *str = va_arg(args, char *); /* Gets the string from variadic arguments */
+    int length = 0;
+
+    if (str != NULL)
+    {
+        while (*str)
+        {
+            _putchar(*str);
+            str++;
+            length++;
+        }
+        return (length);
+    }
+
+    /* If the string is null, prints "(null)" and returns its length */
+    _putchar('(');
+    _putchar('n');
+    _putchar('u');
+    _putchar('l');
+    _putchar('l');
+    _putchar(')');
+    return (6);
+}
+
+/**
+ * _print_a_integer - Prints an integer
+ * @args: A list of variadic arguments
+ *
+ * Return: The length of the integer's digit string
+ */
+int _print_a_integer(va_list args)
+{
+    int count = 1;
+    int num = 0;
+    unsigned int n = 0;
+
+    n = va_arg(args, int); /* Gets the integer from variadic arguments */
+    num = n;
+    if (num < 0)
+    {
+        _putchar('-');
+        num = -num;
+        n = num;
+        count++;
+    }
+    while (n > 9)
+    {
+        n = n / 10;
+        count++;
+    }
+    _recursion_integer(num);
+    return (count); /* Returns the count of digits of the integer */
+}
+
+/**
+ * _recursion_integer - Prints an integer recursively
+ * @num: The integer to print
+ *
+ * Return: Nothing
+ */
+void _recursion_integer(int num)
+{
+    unsigned int digit;
+
+    digit = num;
+    if (digit / 10)
+        _recursion_integer(digit / 10); /* Calls the function recursively with the next digit */
+    _putchar(digit % 10 + '0');
+}
