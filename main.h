9d0020e6e005c8fe64ade4401bbdcd030db7d725
ivanpachecos <ivanpachecos@yahoo.com>
2024-04-01 01:57:11 -0400

    update file

--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 #include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
   * struct specifiers - Struct specifiers
@@ -12,19 +14,17 @@ typedef struct specifiers
 {
 	char *specifier;
 	int (*f)(va_list args);
-} spc_dt;
+} fmt_spc;
 
 int _putchar(char c);
+int print_format(const char *format, va_list args);
+int _print_invalid_spec(char prev_format, char format, int count);
 int _printf(const char *format, ...);
 int print_char(va_list args);
 int _print_a_string(va_list args);
-int print_format(const char *format, va_list args);
 int _print_spec(char format, va_list args);
-int _print_invalid_spec(char prev_format, char format, int count);
 int _print_a_integer(va_list args);
 void _recursion_integer(int a);
-int _print_int_binary(va_list args);
-void _recursion_int_binary(int a);
 int _validate_char(char _type);
 
 #endif
